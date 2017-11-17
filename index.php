b7ca18ae0a208722a46b9af1ed5f6cf407247fad
dogbert831 <miclarke@nwcybernet.com>
2017-11-17 20:49:23 +0000

    use ajax to update gpio status

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <meta http-equiv="refresh" content="30">
         <title>Raspberry Pi Gpio on Dev</title>
         <style>
         	.img-valign {
@@ -77,19 +76,29 @@
 			  border-radius: 50%;
 			}
     	</style>
-
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     	<script>
     		function exefunction(){
-    			var armed = document.getElementById("alarmset").checked;
+    			var armed = $('#alarmset').checked;
     			console.log(armed);
     		}
+    		function loadstatus(){
+                $('#status').load('status.php',function () {
+                    //setTimeout(loadstatus, 5000);
+                });
+            }
+            setInterval(loadstatus, 5000);
+            loadstatus(); // This will run on page load
+            /*setInterval(function(){
+                loadstatus() // this will run after every 5 seconds
+            }, 10000);*/
     	</script>
     </head>
  
     <body style="background-color: black;">
     <div style="color:white; margin-left: 10%;">
     <img align="right;">
-    <div>
+    <div id="status">
     <!-- On/Off button's picture -->
 	<?php
 	use PHPMailer\PHPMailer\PHPMailer;
@@ -112,24 +121,7 @@
 		$armed = false;
 	}
 	fclose($handle);
-    $zones = array("Front/Garage Door", "Liv Rm Window", "Family Rm Window", "Front BR Window");
-	//this php script generate the first page in function of the file
-	for ( $i= 0; $i<4; $i++) {
-		//set the pin's mode to input and read them
-		//system("gpio mode ".$i."out");
-		//system("gpio mode ".$i."up");
-		//system("gpio write ".$i."1");
-		exec ("gpio read ".$i, $status, $return );
-		//print_r($status);
-        //unset($status);
-	}
-	
-	//for loop to read the value
-	$i =0;
-	for ($i = 0; $i < 4; $i++) {
-		//if off
-		if ($status[$i] == 0 ) {
-			echo ("<span class='text'>$zones[$i]</span><img class='img-valign' src='data/img/red/red.jpg'/></br>");
+    
 			if ($armed == true){
 				$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 				try {
@@ -155,14 +147,9 @@
 				    echo 'Mailer Error: ' . $mail->ErrorInfo;
 				}
 			}
-		}
+		
 	
-		//if on
-		if ($status[$i] == 1 ) {
-			echo ("<span class='text'>$zones[$i]</span><img class='img-valign' src='data/img/green/green.jpg'/></br>");
-			//print_r($status[$i]);
-		}	 
-	}
+	
 	?>
 	</div> 
 
--- status.php
+++ status.php
@@ -3,24 +3,38 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Raspberry Pi Gpio</title>
+        <title>Raspberry Pi Gpio Status</title>
+        
     </head>
  
     <body style="background-color: white;">
-    	<?php
-    		for ( $i= 0; $i<3; $i++) {
-				//set the pin's mode to input and read them
-				exec ("gpio read ".$i, $status, $return );
-				echo $i;
-				print_r($status);
-				echo "</br>";
-		        unset($status);
-			}
-
-
-
-    	?>
-
+        <div id="status">
+    <!-- On/Off button's picture -->
+    <?php
+    $zones = array("Front/Garage Door", "Liv Rm Window", "Family Rm Window", "Front BR Window");
+    for ( $i= 0; $i<4; $i++) {
+        exec ("gpio read ".$i, $status, $return );
+        //print_r($status);
+        //unset($status);
+    }
+    
+    //for loop to read the value
+    $i =0;
+    for ($i = 0; $i < 4; $i++) {
+        //if off
+        if ($status[$i] == 0 ) {
+            echo ("<span class='text'>$zones[$i]</span><img class='img-valign' src='data/img/red/red.jpg'/></br>");
+            
+        }
+    
+        //if on
+        if ($status[$i] == 1 ) {
+            echo ("<span class='text'>$zones[$i]</span><img class='img-valign' src='data/img/green/green.jpg'/></br>");
+            //print_r($status[$i]);
+        }    
+    }
+    ?>
+    </div>
 
     </body>
 </html>
